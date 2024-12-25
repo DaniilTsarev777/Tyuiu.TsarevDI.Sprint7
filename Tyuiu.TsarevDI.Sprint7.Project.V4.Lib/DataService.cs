ddb04e11eec3b3e828cfe8652ec000ff85960f28
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-12-25 17:32:51 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint7.Project.V4.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Mail;
+using System.Text;
+using static System.Reflection.Metadata.BlobBuilder;
+namespace Tyuiu.TsarevDI.Sprint7.Project.V4.Lib
+{
+    public class DataService
+    {
+        public string[,] GetMatrix(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+
+            int rows = lines.Length;
+            int columns = lines[0].Split(';').Length;
+
+            string[,] matrix = new string[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] values = lines[i].Split(";");
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = values[j];
+                }
+            }
+
+            return matrix;
+        }
+
+    }
+}
+
