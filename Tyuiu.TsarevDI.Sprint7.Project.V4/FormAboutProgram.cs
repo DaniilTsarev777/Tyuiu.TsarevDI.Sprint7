c0f1d35ce9ab4e1568e2d3af62b6e9e5f5088fd5
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-12-27 15:51:58 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint7.Project.V4/FormAboutProgram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tyuiu.TsarevDI.Sprint7.Project.V4
+{
+    public partial class FormAboutProgram : Form
+    {
+        public FormAboutProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormAboutProgram_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutProgramText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutProgramHead_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonExitAboutProgram_TDI_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+    }
+
+}
